2867242bd9e1cf203820cc329604c10307499702
emamaria <emascript@gmail.com>
2023-10-12 16:03:29 +0200

    ok server

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express  = require('express');
+
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+
+app.use( express.static('public'))
+
+
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
