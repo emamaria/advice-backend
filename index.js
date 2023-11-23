e16303f8fe3fba2ff7b02acbc44f7381ae08c677
emamaria <emascript@gmail.com>
2023-11-23 16:55:25 +0100

    advice model

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dbConnection()
 
 
 app.use('/api/users', require('./routes/users'));
-
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server running on port ${process.env.PORT}`)
---
+++ models/advice.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose');
+
+const AdviceSchema = Schema({
+    advice: {
+        type: String,
+        required: true
+    },
+    img: {
+        type:String,
+    },
+    user: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    like: {
+        type: Number,
+        default: 0
+    }
+})
+
+UserSchema.method('toJSON', function(){
+  const {__v,...object } = this.toObject()
+  return object;
+})
+
+module.exports = model('Advice', AdviceSchema)
