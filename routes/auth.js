abab9ee0b3db2ecbf0b034f15c8d6511a2923da1
emamaria <emascript@gmail.com>
2023-11-25 17:22:12 +0100

    user login field validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
-
+const { check } = require('express-validator')
+const {fieldsValidation} = require("../middlewares/fields-validation")
 const {login} = require("../controllers/auth")
 
 const router = Router();
 
 
-router.post('/login', login)
+router.post('/login',[
+    check('email', 'Email is required with correct format').isEmail(),
+    check('password', 'Password requires at least 8 characters that contains 1 number, 1 special character, 1 uppercase and 1 lowercase').isStrongPassword(),
+    ], fieldsValidation, login)
 
 
 
